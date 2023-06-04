566bb2274076e0f2bda6517ed595004a9444e5df
Aucante <sebastien.aucante@gmail.com>
2023-06-04 20:55:05 +0200

    exercice 1

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('ok');
+const compteur = document.querySelector('#compteur');
+let i = 0;
+const increment = function (e) {
+    i++;
+    const span = compteur.querySelector('span');
+    if (span) {
+        span.innerText = i.toString();
+    }
+};
+compteur.addEventListener('click', increment);
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('ok');
+const compteur = <HTMLButtonElement>document.querySelector('#compteur');
+
+let i = 0;
+
+const increment = function (e: Event) {
+    i++;
+    const span = compteur.querySelector('span');
+
+    if (span) {
+        span.innerText = i.toString();
+    }
+}
+
+compteur.addEventListener('click', increment);
